061ebed01f56ef6ac602c09f6428f550fcba11fc
giulipulvi22 <79788825+giulipulvi22@users.noreply.github.com>
2021-04-23 23:21:49 +0200

    Add files via upload

--- Scatta con noi/ScattaConNoi.js
+++ Scatta con noi/ScattaConNoi.js
@@ -122,8 +122,45 @@ function findPlace(event){
 
     let input2 = document.querySelector('#data');
     let data = input2.value;
-    console.log(data);
-    fetch('https://api.ipgeolocation.io/astronomy?apiKey=' + API_KEY + '&location=' + citta + '&date=' + data ).then(onResponse).then(onJson);
+
+    fetch('https://api.ipgeolocation.io/astronomy?apiKey=' + API_KEY + '&location=' + citta + '&date=' + data).then(onResponse).then(onJson);
 }
 const search = document.querySelector('#submit');
 search.addEventListener('click', findPlace);
+
+
+
+//youtube
+var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'kTl2dJHn9_E',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();}
